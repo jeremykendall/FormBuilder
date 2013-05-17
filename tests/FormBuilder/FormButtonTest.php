745d4903866737005be68bd3639f8ea5a526de40
Stephen Yeargin <stephen.yeargin@gmail.com>
2013-05-17 18:05:05 -0500

    Adds Travis-CI support, finishes FormButtonTest

---
+++ tests/FormBuilder/FormButtonTest.php
@@ -0,0 +1,63 @@
+<?php
+namespace FormBuilder;
+
+class FormButtonTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var FormButton
+     */
+    protected $object;
+
+    /**
+     * Sets up the fixture, for example, opens a network connection.
+     * This method is called before a test is executed.
+     */
+    protected function setUp()
+    {
+        $this->object = new FormButton('button_label', 'button_id');
+    }
+
+    /**
+     * Tears down the fixture, for example, closes a network connection.
+     * This method is called after a test is executed.
+     */
+    protected function tearDown()
+    {
+    }
+
+    /**
+     * @covers FormBuilder\FormButton::getLabel
+     */
+    public function testGetLabel()
+    {
+        $this->assertEquals('button_label', $this->object->getLabel(), 'Labels are equal.');
+    }
+
+    /**
+     * @covers FormBuilder\FormButton::getID
+     */
+    public function testGetID()
+    {
+        $this->assertEquals('button_id', $this->object->getId(), 'Ids are equal.');
+    }
+
+    /**
+     * @covers FormBuilder\FormButton::setType
+     */
+    public function testSetType()
+    {
+        $this->assertEmpty($this->object->getType(), 'Type is empty before being set.');
+        $this->object->setType('submit');
+        $this->assertEquals('submit', $this->object->getType(), 'Types are equal.');
+    }
+
+    /**
+     * @covers FormBuilder\FormButton::getType
+     */
+    public function testGetType()
+    {
+        $this->assertEmpty($this->object->getType(), 'Type is empty before being set.');
+        $this->object->setType('submit');
+        $this->assertEquals('submit', $this->object->getType(), 'Types are equal.');
+    }
+}
